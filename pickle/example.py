3c761a2caa18ab8f0d839c0d3dd847bf74322335
Jan Kerševan <jan.kersevan@gmail.com>
2022-09-22 14:36:21 +0200

    "neki"

---
+++ pickle/example.py
@@ -0,0 +1,11 @@
+import pickle
+
+if __name__ == "__main__":
+	l1 = [1, 2, 2, 6, 6, 7]
+
+	h = open("tocka.pkl", 'wb')
+	pickle.dump(l1, h, pickle.HIGHEST_PROTOCOL)
+	h.close()
+
+	with open("tocka2.pkl", 'wb') as h:
+		pickle.dump(l1, h, pickle.HIGHEST_PROTOCOL)
---
+++ pickle/example2.py
@@ -0,0 +1,8 @@
+import pickle
+
+
+if __name__ == "__main__":
+	h = open("tocka.pkl", 'rb')
+	pk = pickle.load(h)
+	h.close()
+	print(pk)
