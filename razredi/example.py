5abed5c53caad8edaf38fccb9b9b0264fc654e21
Jan Kerševan <jan.kersevan@gmail.com>
2022-09-22 13:42:28 +0200

    more stuff

--- razredi/example.py
+++ razredi/example.py
@@ -1,7 +1,7 @@
 
 
 
-class Pravokotnik():
+class Pravokotnik(object):
 	barva = "rdeca"
 	def __init__(self, a, b):
 		print("sem nov pravokotnik :)")
@@ -19,13 +19,16 @@ class Pravokotnik():
 
 class Kvadrat(Pravokotnik):
 	def __init__(self, a):
-		super().__init__(a, a)
+		super(Kvadrat, self).__init__(a, a)
 
+if __name__ == "__main__":
+	prav1 = Pravokotnik(5.0, 4.0)
+	prav2 = Pravokotnik(3.0, 4.0)
+	print(prav1.a)
+	print(prav1.ploscina())
+	print(prav1.obseg())
+	print(prav2.barva)
+	print(prav1.barva)
 
-prav1 = Pravokotnik(5.0, 4.0)
-prav2 = Pravokotnik(3.0, 4.0)
-print(prav1.a)
-print(prav1.ploscina())
-print(prav1.obseg())
-print(prav2.barva)
-print(prav1.barva)
+	kv1 = Kvadrat(4.0)
+	print(kv1.ploscina())
